5a728621bd9a7c009ed0b384054de1f152dd293c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-12-23 23:20:34 +0100

    Move projects to correct directory

---
+++ src/core/DevilDaggersInfo.Core.CriteriaExpression/ExpressionOperatorType.cs
@@ -0,0 +1,9 @@
+namespace DevilDaggersInfo.Core.CriteriaExpression;
+
+public enum ExpressionOperatorType : byte
+{
+	Add = 0,
+	Subtract = 1,
+
+	// TODO: Multiply and Divide? Needs precedence system.
+}
