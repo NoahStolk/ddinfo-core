73d61c08002a1133a9d3d77a25c0822b3f5be620
Noah Stolk <noah.stolk@gmail.com>
2023-04-27 16:27:33 +0200

    Move wiki types to wiki lib (#358)

---
+++ src/core/DevilDaggersInfo.Core.Wiki/GameVersion.cs
@@ -0,0 +1,11 @@
+// ReSharper disable InconsistentNaming
+namespace DevilDaggersInfo.Core.Wiki;
+
+public enum GameVersion
+{
+	V1_0 = 0,
+	V2_0 = 1,
+	V3_0 = 2,
+	V3_1 = 3,
+	V3_2 = 4,
+}
---
+++ src/core/DevilDaggersInfo.Core.Wiki/UpgradeUnlockType.cs
@@ -0,0 +1,7 @@
+namespace DevilDaggersInfo.Core.Wiki;
+
+public enum UpgradeUnlockType
+{
+	Gems = 0,
+	Homing = 1,
+}
